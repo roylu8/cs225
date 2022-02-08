f882146225d4a31401377e92262e6c40f03ec861
root <roylu2@illinois.edu>
2022-02-08 05:47:30 +0000

    finished mp stickers

--- mp_stickers/main.cpp
+++ mp_stickers/main.cpp
@@ -8,19 +8,21 @@ int main()
   // Reminder:
   //   Before exiting main, save your creation to disk as myImage.png
   //
-  Image alma, s1, s2, s3, output;
+  Image alma, s1, s2, s3, s4;
   alma.readFromFile("alma.png");
   s1.readFromFile("balloons.png");
   s2.readFromFile("balloons.png");
   s3.readFromFile("balloons.png");
+  s4.readFromFile("i.png");
 
-  StickerSheet myImage(alma, 3);
+  StickerSheet myImage(alma, 4);
   myImage.addSticker(s1, 100, 150);
   myImage.addSticker(s2, 500, 20);
   myImage.addSticker(s3, 650, 250);
-  output = myImage.render();
+  s4.scale(0.5);
+  myImage.addSticker(s4, 20, 425);
 
-  output.writeToFile("myImage.png");
+  (myImage.render()).writeToFile("myImage.png");
 
   return 0;
 }
