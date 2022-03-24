5cdc7d93789deba72e000e2746701847511abf0c
root <roylu2@illinois.edu>
2022-03-24 16:14:45 +0000

    finished lab avl

--- lab_avl/avltree.cpp
+++ lab_avl/avltree.cpp
@@ -3,21 +3,23 @@
  * Definitions of the binary tree functions you'll be writing for this lab.
  * You'll need to modify this file.
  */
+#include <algorithm>
 
 template <class K, class V>
-V AVLTree<K, V>::find(const K& key) const
+V AVLTree<K, V>::find(const K &key) const
 {
     return find(root, key);
 }
 
 template <class K, class V>
-V AVLTree<K, V>::find(Node* subtree, const K& key) const
+V AVLTree<K, V>::find(Node *subtree, const K &key) const
 {
     if (subtree == NULL)
         return V();
     else if (key == subtree->key)
         return subtree->value;
-    else {
+    else
+    {
         if (key < subtree->key)
             return find(subtree->left, key);
         else
@@ -26,14 +28,18 @@ V AVLTree<K, V>::find(Node* subtree, const K& key) const
 }
 
 template <class K, class V>
-void AVLTree<K, V>::rotateLeft(Node*& t)
+void AVLTree<K, V>::rotateLeft(Node *&t)
 {
     functionCalls.push_back("rotateLeft"); // Stores the rotation name (don't remove this)
     // your code here
+    Node *new_root = t->right;
+    t->right = new_root->left;
+    new_root->left = t;
+    t = new_root;
 }
 
 template <class K, class V>
-void AVLTree<K, V>::rotateLeftRight(Node*& t)
+void AVLTree<K, V>::rotateLeftRight(Node *&t)
 {
     functionCalls.push_back("rotateLeftRight"); // Stores the rotation name (don't remove this)
     // Implemented for you:
@@ -42,64 +48,222 @@ void AVLTree<K, V>::rotateLeftRight(Node*& t)
 }
 
 template <class K, class V>
-void AVLTree<K, V>::rotateRight(Node*& t)
+int AVLTree<K, V>::balance(Node *&t)
+{
+
+    if (t == NULL)
+    {
+        return 0;
+    }
+
+    int height_right;
+    int height_left;
+    if (t->right == NULL)
+    {
+        height_right = -1;
+    }
+    else
+    {
+        height_right = t->right->height;
+    }
+
+    if (t->left == NULL)
+    {
+        height_left = -1;
+    }
+    else
+    {
+        height_left = t->left->height;
+    }
+
+    return height_right - height_left;
+}
+
+template <class K, class V>
+int AVLTree<K, V>::height_(Node *&t)
+{
+    if (t == NULL)
+    {
+        return -1;
+    }
+    else
+    {
+        return 1 + std::max(height_(t->left), height_(t->right));
+    }
+}
+
+template <class K, class V>
+void AVLTree<K, V>::update_height(Node *&t)
+{
+
+    if (t == NULL)
+    {
+        return;
+    }
+
+    t->height = height_(t);
+
+    update_height(t->left);
+    update_height(t->right);
+}
+
+template <class K, class V>
+void AVLTree<K, V>::rotateRight(Node *&t)
 {
     functionCalls.push_back("rotateRight"); // Stores the rotation name (don't remove this)
     // your code here
+    Node *new_root = t->left;
+    t->left = new_root->right;
+    new_root->right = t;
+    t = new_root;
 }
 
 template <class K, class V>
-void AVLTree<K, V>::rotateRightLeft(Node*& t)
+void AVLTree<K, V>::rotateRightLeft(Node *&t)
 {
     functionCalls.push_back("rotateRightLeft"); // Stores the rotation name (don't remove this)
     // your code here
+    rotateRight(t->right);
+    rotateLeft(t);
 }
 
 template <class K, class V>
-void AVLTree<K, V>::rebalance(Node*& subtree)
+void AVLTree<K, V>::rebalance(Node *&subtree)
 {
     // your code here
+    int height_right;
+    int height_left;
+
+    if (balance(subtree) == 2)
+    {
+        if (balance(subtree->right) == 1)
+        {
+            rotateLeft(subtree);
+        }
+        else if (balance(subtree->right) == -1)
+        {
+            rotateRightLeft(subtree);
+        }
+    }
+
+    if (balance(subtree) == -2)
+    {
+        if (balance(subtree->left) == -1)
+        {
+            rotateRight(subtree);
+        }
+        else if (balance(subtree->left) == 1)
+        {
+            rotateLeftRight(subtree);
+        }
+    }
+
+    update_height(this->root);
 }
 
 template <class K, class V>
-void AVLTree<K, V>::insert(const K & key, const V & value)
+void AVLTree<K, V>::insert(const K &key, const V &value)
 {
     insert(root, key, value);
 }
 
 template <class K, class V>
-void AVLTree<K, V>::insert(Node*& subtree, const K& key, const V& value)
+void AVLTree<K, V>::insert(Node *&subtree, const K &key, const V &value)
 {
     // your code here
+
+    if (subtree == NULL)
+    {
+        subtree = new Node(key, value);
+    }
+    if (subtree->key == key)
+    {
+        return;
+    }
+    if (subtree->key < key)
+    {
+        insert(subtree->right, key, value);
+        rebalance(subtree);
+    }
+    else
+    {
+        insert(subtree->left, key, value);
+        rebalance(subtree);
+    }
 }
 
 template <class K, class V>
-void AVLTree<K, V>::remove(const K& key)
+void AVLTree<K, V>::remove(const K &key)
 {
     remove(root, key);
 }
 
 template <class K, class V>
-void AVLTree<K, V>::remove(Node*& subtree, const K& key)
+void AVLTree<K, V>::remove(Node *&subtree, const K &key)
 {
     if (subtree == NULL)
         return;
 
-    if (key < subtree->key) {
+    if (key < subtree->key)
+    {
         // your code here
-    } else if (key > subtree->key) {
+        remove(subtree->left, key);
+        rebalance(subtree);
+    }
+    else if (key > subtree->key)
+    {
         // your code here
-    } else {
-        if (subtree->left == NULL && subtree->right == NULL) {
+        remove(subtree->right, key);
+        rebalance(subtree);
+    }
+    else
+    {
+        if (subtree->left == NULL && subtree->right == NULL)
+        {
             /* no-child remove */
             // your code here
-        } else if (subtree->left != NULL && subtree->right != NULL) {
+            Node *temp = subtree;
+            subtree = NULL;
+            if (temp != NULL)
+            {
+                delete temp;
+            }
+        }
+        else if (subtree->left != NULL && subtree->right != NULL)
+        {
             /* two-child remove */
             // your code here
-        } else {
+            Node *curr = subtree->left;
+            while (curr->right != NULL)
+            {
+                curr = curr->right;
+            }
+
+            subtree->key = curr->key;
+            subtree->value = curr->value;
+
+            remove(subtree->left, curr->key);
+            rebalance(subtree);
+        }
+        else
+        {
             /* one-child remove */
             // your code here
+            Node *temp = subtree;
+            if (subtree->right != NULL)
+            {
+                subtree = subtree->right;
+                delete temp;
+            }
+            else
+            {
+                subtree = subtree->left;
+                delete temp;
+            }
         }
         // your code here
+        rebalance(subtree);
     }
+
+    rebalance(subtree);
 }
--- lab_avl/avltree.h
+++ lab_avl/avltree.h
@@ -19,219 +19,226 @@
 template <class K, class V>
 class AVLTree
 {
-  private:
-    /**
-     * Node represents a tree node; that is, an element in a AVLTree.
-     * It stores a key, value, and pointers to its left and right children.
-     */
-    struct Node {
-        K key;
-        V value;
-        Node* left;
-        Node* right;
-        int height;
-
-        /**
-         * Node constructor; sets children to point to `NULL`.
-         * @param newKey The object to use as a key
-         * @param newValue The templated data element that the constructed
-         *  node will hold.
-         */
-        Node(const K& newKey, const V& newValue)
-            : key(newKey), value(newValue), left(NULL), right(NULL), height(0)
-        {
-        }
-    };
-
-  public:
-    /**
-     * Constructor to create an empty tree.
-     */
-    AVLTree();
+private:
+  /**
+   * Node represents a tree node; that is, an element in a AVLTree.
+   * It stores a key, value, and pointers to its left and right children.
+   */
+  struct Node
+  {
+    K key;
+    V value;
+    Node *left;
+    Node *right;
+    int height;
 
     /**
-     * Copy constructor.
-     * @param other The tree to copy
+     * Node constructor; sets children to point to `NULL`.
+     * @param newKey The object to use as a key
+     * @param newValue The templated data element that the constructed
+     *  node will hold.
      */
-    AVLTree(const AVLTree& other);
+    Node(const K &newKey, const V &newValue)
+        : key(newKey), value(newValue), left(NULL), right(NULL), height(0)
+    {
+    }
+  };
 
-    /**
-     * Destructor; frees all nodes associated with this tree.
-     */
-    ~AVLTree();
+public:
+  /**
+   * Constructor to create an empty tree.
+   */
+  AVLTree();
 
-    /**
-     * Assignment operator.
-     * @param rhs The tree to copy
-     * @return A reference to the current tree
-     */
-    const AVLTree& operator=(const AVLTree& rhs);
+  /**
+   * Copy constructor.
+   * @param other The tree to copy
+   */
+  AVLTree(const AVLTree &other);
 
-    /**
-     * Frees all nodes associated with this tree and sets it to be empty.
-     */
-    void clear();
+  /**
+   * Destructor; frees all nodes associated with this tree.
+   */
+  ~AVLTree();
+  
+  void update_height(Node *&tree);
 
-    /**
-     * Inserts a key and value into the AVLTree.
-     * @param key The key to insert
-     * @param value The value associated with the key
-     */
-    void insert(const K& key, const V& value);
+  /**
+   * Assignment operator.
+   * @param rhs The tree to copy
+   * @return A reference to the current tree
+   */
+  const AVLTree &operator=(const AVLTree &rhs);
 
-    /**
-     * Removes a key from the AVLTree. The key is assumed to exist in the tree.
-     * @param key The key to remove
-     */
-    void remove(const K& key);
+  /**
+   * Frees all nodes associated with this tree and sets it to be empty.
+   */
+  void clear();
 
-    /**
-     * Finds an element in the AVL tree.
-     * @param key The element to search for
-     * @return The value stored for that key
-     */
-    V find(const K& key) const;
+  int balance(Node *&t);
 
-    /**
-     * Prints the function calls to a stream.
-     * @param out The stream to print to (default is stdout)
-     */
-    void printFunctionOrder(std::ostream& out = std::cout) const;
+  int height_(Node *&t);
 
-    /**
-     * Prints the AVLTree to a stream.
-     * @param out The stream to print to (default is stdout)
-     */
-    void print(std::ostream& out = std::cout, bool order = true) const;
+  /**
+   * Inserts a key and value into the AVLTree.
+   * @param key The key to insert
+   * @param value The value associated with the key
+   */
+  void insert(const K &key, const V &value);
 
-    /**
-     * This function is used for grading.
-     * @param newOut The stream to print to
-     */
-    void setOutput(std::ostream& newOut);
+  /**
+   * Removes a key from the AVLTree. The key is assumed to exist in the tree.
+   * @param key The key to remove
+   */
+  void remove(const K &key);
 
-    /**
-     * Gets the in-order traversal of an AVL tree's keys.
-     */
-    std::vector<K> getInorderTraversal() const;
+  /**
+   * Finds an element in the AVL tree.
+   * @param key The element to search for
+   * @return The value stored for that key
+   */
+  V find(const K &key) const;
 
-    /**
-     * Gets the pre-order traversal of an AVL tree's keys.
-     */
-    std::vector<K> getPreorderTraversal() const;
+  /**
+   * Prints the function calls to a stream.
+   * @param out The stream to print to (default is stdout)
+   */
+  void printFunctionOrder(std::ostream &out = std::cout) const;
 
-    /*
-    * Gets vector of function calls.
-    */
-    std::vector<std::string> getFunctionOrder() const;
+  /**
+   * Prints the AVLTree to a stream.
+   * @param out The stream to print to (default is stdout)
+   */
+  void print(std::ostream &out = std::cout, bool order = true) const;
 
-  private:
-    /**
-     * The root of the tree.
-     */
-    Node* root;
+  /**
+   * This function is used for grading.
+   * @param newOut The stream to print to
+   */
+  void setOutput(std::ostream &newOut);
 
-    /**
-     * Private helper function for the public #insert function.
-     * @param node The current node in the recursion
-     * @param key The key to insert
-     * @param value The value associated with the key
-     */
-    void insert(Node*& node, const K& key, const V& value);
+  /**
+   * Gets the in-order traversal of an AVL tree's keys.
+   */
+  std::vector<K> getInorderTraversal() const;
 
-    /**
-     * Private helper function for the public #remove function.
-     * @param node The current node in the recursion
-     * @param key The key to remove
-     */
-    void remove(Node*& node, const K& key);
+  /**
+   * Gets the pre-order traversal of an AVL tree's keys.
+   */
+  std::vector<K> getPreorderTraversal() const;
 
-    /**
-     * Finds a value (by key) in the AVL tree.
-     * @param node The node to search from (current subroot)
-     * @param key The key to search for
-     * @return The value stored for that key
-     */
-    V find(Node* node, const K& key) const;
+  /*
+   * Gets vector of function calls.
+   */
+  std::vector<std::string> getFunctionOrder() const;
 
-    /**
-     * Rotate the tree right (there is an imbalance on the left side).
-     * @param node The node to rotate
-     */
-    void rotateRight(Node*& node);
+private:
+  /**
+   * The root of the tree.
+   */
+  Node *root;
 
-    /**
-     * Rotates the tree left (there is an imbalance on the right side).
-     * @param node The node to rotate
-     */
-    void rotateLeft(Node*& node);
+  /**
+   * Private helper function for the public #insert function.
+   * @param node The current node in the recursion
+   * @param key The key to insert
+   * @param value The value associated with the key
+   */
+  void insert(Node *&node, const K &key, const V &value);
 
-    /**
-     * A right-left rotation.
-     * This function should simply call rotateRight and rotateLeft.
-     * @param node The node to rotate
-     */
-    void rotateRightLeft(Node*& node);
+  /**
+   * Private helper function for the public #remove function.
+   * @param node The current node in the recursion
+   * @param key The key to remove
+   */
+  void remove(Node *&node, const K &key);
 
-    /**
-     * A left-right rotation.
-     * This function should simply call rotateLeft and rotateRight.
-     * @param node The node to rotate
-     */
-    void rotateLeftRight(Node*& node);
+  /**
+   * Finds a value (by key) in the AVL tree.
+   * @param node The node to search from (current subroot)
+   * @param key The key to search for
+   * @return The value stored for that key
+   */
+  V find(Node *node, const K &key) const;
 
-    /**
-     * Rebalance a node by performing rotations. You can assume that node->left
-     * and node->right are both balanced. Even if no rotations are required,
-     * you should update the node's height.
-     * @param node The node to balance.
-     */
-    void rebalance(Node*& node);
+  /**
+   * Rotate the tree right (there is an imbalance on the left side).
+   * @param node The node to rotate
+   */
+  void rotateRight(Node *&node);
 
-    /**
-     * Return the node->height if the node is not NULL. Otherwise return -1.
-     * Note: this function does not calculate the **actual** height of a node.
-     * @param node The node's height to check
-     * @return The height of the node if it's non-`NULL` or -1 if it is `NULL`
-     */
-    int heightOrNeg1(const Node* node) const;
+  /**
+   * Rotates the tree left (there is an imbalance on the right side).
+   * @param node The node to rotate
+   */
+  void rotateLeft(Node *&node);
 
-    /**
-     * Swap the keys and values of two nodes.
-     * @param first The first node to swap
-     * @param second The node to swap with
-     */
-    void swap(Node*& first, Node*& second);
+  /**
+   * A right-left rotation.
+   * This function should simply call rotateRight and rotateLeft.
+   * @param node The node to rotate
+   */
+  void rotateRightLeft(Node *&node);
 
-    /**
-     * Helper function for #operator= and AVLTree(const AVLTree &).
-     * @param subRoot The current node in the recursion
-     */
-    Node* copy(const Node* subRoot);
+  /**
+   * A left-right rotation.
+   * This function should simply call rotateLeft and rotateRight.
+   * @param node The node to rotate
+   */
+  void rotateLeftRight(Node *&node);
 
-    /**
-     * Private helper function for clear that clears beneath the parameter node.
-     * @param subRoot The current node in the recursion
-     */
-    void clear(Node* subRoot);
+  /**
+   * Rebalance a node by performing rotations. You can assume that node->left
+   * and node->right are both balanced. Even if no rotations are required,
+   * you should update the node's height.
+   * @param node The node to balance.
+   */
+  void rebalance(Node *&node);
 
-    /**
-     * Gets the in-order traversal of an AVL tree's keys.
-     * @param subRoot The current node in the recursion
-     */
-    void getInorderTraversal(const Node* subRoot, std::vector<K>& traversal) const;
+  /**
+   * Return the node->height if the node is not NULL. Otherwise return -1.
+   * Note: this function does not calculate the **actual** height of a node.
+   * @param node The node's height to check
+   * @return The height of the node if it's non-`NULL` or -1 if it is `NULL`
+   */
+  int heightOrNeg1(const Node *node) const;
 
-    /**
-     * Gets the pre-order traversal of an AVL tree's keys.
-     * @param subRoot The current node in the recursion
-     */
-    void getPreorderTraversal(const Node* subRoot, std::vector<K>& traversal) const;
+  /**
+   * Swap the keys and values of two nodes.
+   * @param first The first node to swap
+   * @param second The node to swap with
+   */
+  void swap(Node *&first, Node *&second);
 
-    /** This variable is used for grading. */
-    std::ostream* _out;
+  /**
+   * Helper function for #operator= and AVLTree(const AVLTree &).
+   * @param subRoot The current node in the recursion
+   */
+  Node *copy(const Node *subRoot);
 
-    /** This variable tests the order of function calls **/
-    std::vector<std::string> functionCalls;
+  /**
+   * Private helper function for clear that clears beneath the parameter node.
+   * @param subRoot The current node in the recursion
+   */
+  void clear(Node *subRoot);
+
+  /**
+   * Gets the in-order traversal of an AVL tree's keys.
+   * @param subRoot The current node in the recursion
+   */
+  void getInorderTraversal(const Node *subRoot, std::vector<K> &traversal) const;
+
+  /**
+   * Gets the pre-order traversal of an AVL tree's keys.
+   * @param subRoot The current node in the recursion
+   */
+  void getPreorderTraversal(const Node *subRoot, std::vector<K> &traversal) const;
+
+  /** This variable is used for grading. */
+  std::ostream *_out;
+
+  /** This variable tests the order of function calls **/
+  std::vector<std::string> functionCalls;
 };
 
 #include "avltree_given.cpp"
